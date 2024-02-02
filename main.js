b87e3d83a021420cc5d5216fdaade7d22478e0f2
KamuiSiro <I.Kamui.kun@gmail.com>
2024-02-02 14:49:09 +0300

    The Game

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-var snakeSize = 2;
-const initialSnakeX = width-20;
-const initialSnakeY = height/2;
+var snakeSize = 1;
+var snakeBody = [];
+var initialSnakeX = width-20;
+var initialSnakeY = 20;
+var init = true;
+var snakeSpeed = 3;
 
 function random(min, max) {
 
@@ -19,158 +22,198 @@ function randomRGB() {
     if (color === 'rgb(255,0,0)') color = randomRGB();
     return color;
 }
+class Dot {
 
-
-class Food {
-
-    constructor(x, y, color, size) {
+    constructor (x, y, color) {
 
         this.x = x;
         this.y = y;
         this.color = color;
-        this.size = size;
-        this.exists = true;
-        this.snakeDots = [];
+        this.size = 10;
     }
-    checkBounds() {
 
-        if (this.x + this.size >= width) {
-          this.x -= this.size;
-        }
-      
-        if (this.x - this.size <= 0) {
-          this.x += this.size;
-        }
-      
-        if (this.y + this.size >= height) {
-          this.y -= this.size;
-        }
-      
-        if (this.y - this.size <= 0) {
-          this.y += this.size;
-        }     
-    };
-    
     draw() {
-        
+
         ctx.beginPath();
         ctx.fillStyle = this.color;
-        if (this.x + this.size >= width) this.x -= this.size;
-        if (this.x - this.size <= 0) this.x += this.size;
-        if (this.y + this.size >= height) this.y -= this.size;
-        if (this.y - this.size <= 0) this.y += this.size;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     };
+}
+
+class Food  extends Dot {
+
+    constructor(x, y, color) {
+
+        super(x, y, color);
+        this.exists = true;
+    }
 
     catchDetect() {
         
-        if (!(this === snakeHead)) {
-            var dx = this.x - snakeHead.x;
-            var dy = this.y - snakeHead.y;
-            var distance = Math.sqrt(dx * dx + dy * dy);
-      
-            if (distance < this.size + snakeHead.size) {
-                this.exists = false;
-                snakeSize++;
-            }
+        var dx = this.x - snakeBody[0].x;
+        var dy = this.y - snakeBody[0].y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + snakeBody[0].size) {
+            this.exists = false;
+            snakeSize++
+            var tmp = snake.moveTo;
+            snake = new Snake(snakeSize, snakeSpeed);
+            snake.moveTo = tmp;
         }
     };
 }
 
-class Snake extends Food {
+class Snake extends Dot {
 
-    constructor(head, length) {
+    constructor(length, speed) {
 
-        super(initialSnakeX, initialSnakeY, "red", 10);
+        super(initialSnakeX, initialSnakeY, "red");
         this.length = length;
-        this.head = head;
-        this.moveTo = 0;
-        this.speed = 4;
+        this.moveTo;
+        this.speed = speed;
+        this.end = true;
 
 
         window.addEventListener('keydown', (e) => {
+
             switch (e.key) {
               case 'a':
                 this.moveTo = 4; //west
+                init = false;
                 break;
               case "d":
                 this.moveTo = 2; //east
+                init = false;
                 break;
               case "s":
                 this.moveTo = 3; //south
+                init = false;
                 break;
               case "w":
                 this.moveTo = 1; //north
+                init = false;
                 break;
             }
           });
     }
 
     drawSnake() {
-        
-        
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        for (var i = 0; i < length; i++) {
 
-            var dot = new Food(this.x, this.y, "red", 10);
-            snakeDots.push(dot);            
+        var tmp = this.y;
+        for (var i = this.length; i>0; i--) {
+
+            var dot = new Dot(this.x, tmp, this.color);
+            tmp += this.size*2;
+            snakeBody.push(dot);
         }
-        for (var body of snakeDots) {
-            if (this.x + this.size >= width) this.x -= this.size;
-            if (this.x - this.size <= 0) this.x += this.size;
-            if (this.y + this.size >= height) this.y -= this.size;
-            if (this.y - this.size <= 0) this.y += this.size;
-            ctx.arc(body.x, body.y, 10, 0, 2*Math.PI);
-            ctx.fill();
-        }
+        for (var dots of snakeBody) dots.draw();
     }
 
     move() {
         
         if (this.moveTo === 4) {
             
-            this.x -= this.speed;
-            this.drawSnake();
+            for (var i = this.length; i >= 0; i-- ) {
+
+                if (i === 0) {
+                    snakeBody[i].x -= this.speed;
+                }
+                else {
+                    snakeBody[i].x = snakeBody[i-1].x;
+                    snakeBody[i].y = snakeBody[i-1].y;
+                } 
+                snakeBody[i].draw();
+            }
         }
         
         if (this.moveTo === 2) {
 
-            this.x += this.speed;
-            this.drawSnake();
+            for (var i = this.length; i >= 0; i-- ) {
+
+                if (i === 0) {
+                    snakeBody[i].x += this.speed;
+                }
+                else {
+                    snakeBody[i].x = snakeBody[i-1].x;
+                    snakeBody[i].y = snakeBody[i-1].y;
+                } 
+                snakeBody[i].draw();
+            }
         }
 
         if (this.moveTo === 1) {
 
-            this.y -= this.speed;
-            this.drawSnake();
+            for (var i = this.length; i >= 0; i-- ) {
+
+                if (i === 0) {
+                    snakeBody[i].y -= this.speed;
+                }
+                else {
+                    snakeBody[i].x = snakeBody[i-1].x;
+                    snakeBody[i].y = snakeBody[i-1].y;
+                } 
+                snakeBody[i].draw();
+            }
         }
 
         if (this.moveTo === 3) {
 
-            this.y += this.speed;
-            this.drawSnake();
-        }
+            for (var i = this.length; i >= 0; i-- ) {
+
+                if (i === 0) {
+                    snakeBody[i].y += this.speed;
+                }
+                else {
+                    snakeBody[i].x = snakeBody[i-1].x;
+                    snakeBody[i].y = snakeBody[i-1].y;
+                } 
+                snakeBody[i].draw();
+            }
+        };
+    };
+
+    crashDetect() {
+        
+        var crashX = snakeBody[0].x - this.size;
+        var crashY = snakeBody[0].y - this.size;
+        if (crashX <= 0 || crashX >= width || crashY <= 0 || crashY >= height) this.end = false;
     }
 
 }
 
-const snakeHead = new Food(random(0 + 10, width - 10), random(0 + 10, height - 10), "red", 10);
+var snake = new Snake(snakeSize, snakeSpeed);
 var food = new Food(random(0 + 10, width - 10), random(0 + 10, height - 10), randomRGB(), 10);
-var snake = new Snake(snakeHead, snakeSize);
+
 
 function game() {
 
     ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
     ctx.fillRect(0, 0, width, height);
-    food.draw();
-    if (snake.moveTo === 0) {
-        snake.draw();        
+    if (food.exists) {
+        food.draw();
+    }
+    else food = new Food(random(0 + 10, width - 10), random(0 + 10, height - 10), randomRGB(), 10);
+    if (init) {
+        snake.drawSnake();        
     }
     else {
         snake.move();
     }
-    requestAnimationFrame(game);
+    food.catchDetect();
+    snake.crashDetect();
+    var go = requestAnimationFrame(game);
+    if (!snake.end) {
+        alert("Game over. Your score is " + (snakeSize-1));
+        snakeBody.length = 0;
+        snake.moveTo = undefined;
+        cancelAnimationFrame(go);
+        location.reload();
+    }
 }
 game();
+
+function speedUp() {
+    snakeSpeed = snakeSpeed*2;
+}
+setInterval(speedUp, 60*1000);
